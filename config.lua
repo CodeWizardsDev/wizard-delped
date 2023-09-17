02cdb81e9feb04c7a2833a36d7e86205ffa9a429
Code Wizards <94300419+CodeWizardsDev@users.noreply.github.com>
2023-09-17 11:21:56 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,7 +6,9 @@ Config = {
     CoolDown = true,                                                                 -- Should We Use CoolDown For Commands? true or false
     CoolDownTime = 10,                                                               -- If `CoolDown` Is true, How Much Seconds Will Be Cooldown?
 
-    DeadOnly = true,                                                                 -- Should We Remove Dead Peds Only? true, false
+    DeadOnly = false,                                                                -- Should We Remove Dead Peds Only? true, false
+    DelDriver = true,                                                                -- Should We Remove Driver Peds? true, false (IF DEADONLY IS SET TO FALSE)
+    DelWalking = true,                                                               -- Should We Remove Walking Peds? true, false (IF DEADONLY IS SET TO FALSE)
 
     Distance = 100.0,                                                                -- Default Distance To Remove Peds If No Args Used In Command
 
